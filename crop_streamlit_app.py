927fb34a77bf2d4e080cd5440f63096e3cb13365
Shaziya Sultana <shaziyasultana252@gmail.com>
2024-08-07 11:58:59 +0530

    Add files via upload

---
+++ crop_streamlit_app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import nbformat
+from nbconvert import PythonExporter
+from io import StringIO
+import contextlib
+
+def run_notebook(notebook_path):
+    with open(notebook_path) as f:
+        notebook = nbformat.read(f, as_version=4)
+        
+    exporter = PythonExporter()
+    source, meta = exporter.from_notebook_node(notebook)
+    
+    buffer = StringIO()
+    with contextlib.redirect_stdout(buffer):
+        exec(source, globals())
+    
+    return buffer.getvalue()
+
+st.title("Jupyter Notebook Viewer")
+
+notebook_path = 'TFT.ipynb'  # Path to the notebook file in the GitHub repo
+try:
+    notebook_output = run_notebook(notebook_path)
+    st.text(notebook_output)
+except Exception as e:
+    st.error(f"Error running notebook: {e}")
+
