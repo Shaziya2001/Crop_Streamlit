6ecd26377a6587badb1cafefc9c6859a805e542e
Shaziya Sultana <shaziyasultana252@gmail.com>
2024-08-07 11:48:21 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,29 @@
+import nbformat
+from nbconvert import PythonExporter
+from io import StringIO
+import contextlib
+import streamlit as st
+import sys
+
+def run_notebook(notebook_path):
+    with open(notebook_path) as f:
+        notebook = nbformat.read(f, as_version=4)
+        
+    exporter = PythonExporter()
+    source, meta = exporter.from_notebook_node(notebook)
+    
+    buffer = StringIO()
+    with contextlib.redirect_stdout(buffer):
+        exec(source, globals())
+    
+    return buffer.getvalue()
+
+notebook_path = '/content/TFT.ipynb'  
+
+st.title("Jupyter Notebook Viewer")
+
+try:
+    notebook_output = run_notebook(notebook_path)
+    st.text(notebook_output)
+except Exception as e:
+    st.error(f"Error running notebook: {e}")
